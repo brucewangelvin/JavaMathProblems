ad659e597e29f6f88887a4ee658b3bffcf5d8561
Dark-Knight <yanling.elvin@gmail.com>
2019-03-01 16:19:59 -0500

    Create Loops.java

---
+++ Loops.java
@@ -0,0 +1,11 @@
+
+public class Loops {
+    public static void main(String[] args) {
+        System.out.println("Output is : ");
+        for(int i = 1; i <=1000; i++)
+            System.out.println(i);
+
+
+    }
+}
+
